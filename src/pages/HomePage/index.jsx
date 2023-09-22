0c93ff607200f87980949f3a20162a48c0ca82e8
Анударь Оюунбат <LP230018@G301-CC0132.local>
2023-09-22 17:50:56 +0800

    nemelt

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -20,31 +20,32 @@ function Home() {
             transition={{ duration: 1 }}
           >
             <section className={styles.Intro}>
-              <h3 className="text-3xl text-slate-500 sm:text-base ">Hello</h3>
+              <h3 className="text-[25px] text-slate-500 sm:text-base ">
+                Hello
+              </h3>
               <h1 className={styles.myName}>My name is Anudari Oyunbat</h1>
-              <div className="flex flex-col items-center content-center">
-                <div className="flex">
-                  <h4 className={styles.about}>I'm </h4>
-                  <h4 className={styles.maj}>
-                    <Typewriter
-                      words={[
-                        "a UI/UX Designer",
-                        "a Backend developer",
-                        "a Frontend developer",
-                        "a Full stack developer",
-                      ]}
-                      loop={true}
-                      cursor
-                      cursorStyle="|"
-                      typeSpeed={70}
-                      deleteSpeed={50}
-                      delaySpeed={1000}
-                    />
-                  </h4>
-                </div>
-                <Footer />
-                <Button />
+
+              <div className="flex">
+                <h4 className={styles.about}>I'm </h4>
+                <h4 className={styles.maj}>
+                  <Typewriter
+                    words={[
+                      "a UI/UX Designer",
+                      "a Backend developer",
+                      "a Frontend developer",
+                      "a Full stack developer",
+                    ]}
+                    loop={true}
+                    cursor
+                    cursorStyle="|"
+                    typeSpeed={70}
+                    deleteSpeed={50}
+                    delaySpeed={1000}
+                  />
+                </h4>
               </div>
+              <Footer />
+              <Button />
             </section>
           </motion.div>
           <motion.div
